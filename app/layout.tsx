d3b9160f4d0a9a2dda67440ff4a7b98832c42510
zachrynewycz <hrynewyczz@gmail.com>
2023-11-26 19:57:37 -0600

    remove nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Nav from "./components/Nav";
 import AppWrapper from "./AppWrapper";
 
 export const metadata: Metadata = {
@@ -12,10 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body>
-                <AppWrapper>
-                    <Nav />
-                    {children}
-                </AppWrapper>
+                <AppWrapper>{children}</AppWrapper>
             </body>
         </html>
     );
